8381f45a7bab4295a5d9bb198d24413751fb8c01
Joshua Walton <joash.tron@gmail.com>
2019-12-12 17:57:22 -0600

    Souls Have Gender

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_235337) do
+ActiveRecord::Schema.define(version: 2019_12_12_235536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_12_12_235337) do
     t.string "unique_identifier"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "gender"
   end
 
   create_table "users", force: :cascade do |t|
